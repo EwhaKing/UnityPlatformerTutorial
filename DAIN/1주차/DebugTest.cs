21b90c91cf564c836b47671553dc4ff2e08365b4
TIME Desktop <newspaper0227@gmail.com>
2024-03-31 00:11:23 +0900

    1주차 / 유니티 기초, 유니티 2D 기초(생성 함수까지)

---
+++ DAIN/1주차/DebugTest.cs
@@ -0,0 +1,10 @@
+// 사용되지 않는 건 회색 글씨로 바뀜. 그래서 삭제해도 무방함.
+using UnityEngine;
+
+public class DebugTest : MonoBehaviour // 클래스명과 파일명 반드시 일치하도록
+{
+    private void Awake()
+    {
+        Debug.Log("스터디 0주차");
+    }
+}
