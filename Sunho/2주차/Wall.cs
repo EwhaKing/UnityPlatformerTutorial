99c44980e93e96ae9fb48cdefa09a025aa23485a
Sunho12 <113418806+Sunho12@users.noreply.github.com>
2024-04-11 16:48:12 +0900

    Create Wall.cs

---
+++ Sunho/2주차/Wall.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    private SpriteRenderer SpriteRenderer;
+
+    private void Awake()
+    {
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 벽에 부딪힌 오브젝트 삭제
+        Destroy(collision.gameObject);
+
+        // 충돌이 일어나면 벽의 색상을 잠깐 변경
+        StartCoroutine("HitAnimation");
+    }
+
+    private IEnumerator HitAnimation()
+    {
+        SpriteRenderer.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        SpriteRenderer.color = Color.white; 
+    }
+
+}
