894801933abdaf25e22950c0d7ae3eb9362324aa
rnrgll <shuajjjjj@naver.com>
2024-04-12 02:17:05 +0900

    3주차 / 오브젝트 이동을 제어하는 스크립트 생성

---
+++ DoHyun/Unity2D_Platformer/Assets/Scripts/MovementRigidbody2D.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementRigidbody2D : MonoBehaviour
+{
+
+    //[Header("Text")] : 괄호 내부 문자열을 인스펙터 창에 굵은 글씨로 표시해준다.
+    [Header("LayerMask")]
+    [SerializeField]
+    private LayerMask groundCheckLayer; //바닥 체크를 위한 충돌 레이어
+
+    [Header("Move")]
+    [SerializeField]
+    private float walkSpeed = 5; // 걷는 속도
+    [SerializeField]
+    private float runSpeed = 8; //달리는 속도
+
+    [Header("Jump")]
+    [SerializeField]
+    private float jumpForce = 13; //점프 힘
+    [SerializeField]
+    private float lowGravityScale = 2; //높은 점프(점프 키 오래 누르고 있을 때) -> gravity sacale이 적을 수록 중력의 영향을 적게 받는것!
+    [SerializeField]
+    private float highGravityScale = 3.5f; //낮은 점프(일반 점프)
+
+    private float moveSpeed; //이동 속도
+    private Vector2 collisionSize; //머리, 발 위치에 생성하는 충돌 박스 크기
+    private Vector2 footPosition; //발 위치
+
+    private Rigidbody2D rigidbody2D;
+    private Collider2D collider2D;
+
+    public bool IsLongJump { set; get; } = false; //낮은 점프, 높은 점프 체크
+    public bool IsGrounded { set; get; } = false; //바닥 체크
+
+
+    private void Awake()
+    {
+        //초기 : 이동 속도를 걷는 속도로 설정
+        moveSpeed = walkSpeed;
+        //오브젝트의 컴포넌트를 사용하기 위해서는
+        //프로그램이 실행되면 가장 먼저 호출되는 awake 함수에서 GetComponent 메소드를 사용해서 컴포넌트를 가져와야 한다.
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        collider2D = GetComponent<Collider2D>();
+    }
+
+    /// <summary>
+    /// x축 속력(velocity)을 설정하는 메소드. 외부 클래스에서 호출
+    /// </summary>
+    /// <param name="x"></param>
+    public void MoveTo(float x)
+    {
+        //Point : 움직이게 한다, 속력을 조절한다 -> rigidbody를 사용해 컨트롤하자.
+
+
+        //움직이는 속도 = 절대값이 0.5면 걷기, 1이면 뛰기.
+        moveSpeed = Mathf.Abs(x) == 0.5 ? walkSpeed : runSpeed;
+
+        //x값의 부호에 따라 이동 병향 결정 (x가 0이면 정지이니까 그 외 경우에만 방향을 결정해주어야 한다.)
+        if (x != 0)
+        {
+            x = Mathf.Sign(x); //인자가 0, 양수면 1 반환 음수면 -1 반환
+        }
+
+        //마지막에 리지드바디의 속도를 바꿀 때 이동 방향을 결정하는 부호를 이동 속도에 곱해준다!
+        //velocity 2차원 벡터로 전달해야한다.
+        rigidbody2D.velocity = new Vector2(x * moveSpeed, rigidbody2D.velocity.y);
+    }
+
+    //플레이어 이동에 따라 coliison 위치가 변경되므로 매번 충돌박스를 생성하고 바닥인지 체크하도록 해야한다.
+    private void UpdateCollision()
+    {
+        Bounds bounds = collider2D.bounds; //플레이어 오브젝트의 Collider2D의 min, max, center의 위치 정보를 가져온다.
+        //이를 통해 머리, 발에 충돌 범위를 설정, 발 위치 설정 -> 바닥 충돌 감지한다.
+
+        //1.충돌 박스 크기 구하기
+        collisionSize = new Vector2((bounds.max.x - bounds.min.x) * 0.5f, 0.1f);
+        //2. 발 위치 설정하기
+        footPosition = new Vector2(bounds.center.x, bounds.min.y);
+        //3. 바닥인지 체크 -> 물리 충돌박스의 overlap?
+        IsGrounded = Physics2D.OverlapBox(footPosition, collisionSize, 0, groundCheckLayer); //groundCheckLayer : 바닥 충돌 체크 레이어
+
+        //Physics2D.OverlapBox(Vector2 point, Vector2 size, float angle, int layerMask)
+        //point 위치에 size 만큼의 충돌 박스(BoxCollider2D)를 angle 각도 만큼 회전해서 생성한다.
+        //이 박스는 layerMask에 설정된 레이어만 충돌 감지한다.
+        //반환 값을 bool로 받으면 충돌 여부에 따라 true/false를 반환하고
+        //Collider2D hitObject로 받으면 충돌한 오브젝트의 Collider 정보를 반환한다. 여러 개의 충돌 오브젝트가 존재하는 경우 OverlapBoxAll 메소드를 사용해 배열로 받으면 된다.
+
+    }
+
+    /// <summary>
+    /// 마찬가지로 다른 클래스에서 호출하는 점프 메소드이다. y축 점프
+    /// </summary>
+    public void Jump()
+    {
+        if (IsGrounded == true)
+        {
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpForce);
+        }
+    }
+
+    //낮은 점프, 높은 점프 구현을 위해 중력 계수를 조절
+    private void JumpHeight()
+    {
+        //longJump이고, 점프 하는 중(올라가는 중)
+        if (IsLongJump && rigidbody2D.velocity.y > 0)
+        {
+            rigidbody2D.gravityScale = lowGravityScale;
+        }
+        else
+        {
+            rigidbody2D.gravityScale = highGravityScale;
+        }
+    }
+
+
+
+    private void Update()
+    {
+        UpdateCollision();
+        JumpHeight();
+    }
+
+}
