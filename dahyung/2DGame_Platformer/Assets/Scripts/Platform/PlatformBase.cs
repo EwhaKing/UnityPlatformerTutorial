7f171cb8472fce9caeac87fc87d178f110843ba2
yongaricode <kang061313@gmail.com>
2024-05-05 20:00:59 +0900

    4주차

---
+++ dahyung/2DGame_Platformer/Assets/Scripts/Platform/PlatformBase.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlatformBase : MonoBehaviour
+{
+    // 플랫폼과 플레이어가 충돌했는지 체크 (일정 시간동안 다시 충돌체크를 하지 않도록)
+    public bool IsHit {protected set; get;} = false;
+
+    public abstract void UpdateCollision(GameObject other);
+}
