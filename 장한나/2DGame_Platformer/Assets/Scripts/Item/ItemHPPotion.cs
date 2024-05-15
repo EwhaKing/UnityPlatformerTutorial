9acc40c920e6ded1e7474b59e3479a642d67381d
Hanna.Jang <wkdgkssk0711@gmail.com>
2024-05-15 14:15:30 +0900

    5주차/장애물,플레이어 기능과 아이템, 플레이어 정보 UI

---
+++ 장한나/2DGame_Platformer/Assets/Scripts/Item/ItemHPPotion.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class ItemHPPotion : ItemBase
+{
+    public override void UpdateCollision(Transform target)
+    {
+        target.GetComponent<PlayerHP>().IncreaseHP();
+  }
+}
