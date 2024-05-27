b83f2092926de85843ad95c8fe54178d59abaa13
rnrgll <shuajjjjj@naver.com>
2024-05-27 17:10:35 +0900

    7주차 / Intro, SelectLevel Scene 구현

--- DoHyun/Unity2D_Platformer/Assets/Scripts/Common/Constants.cs
+++ DoHyun/Unity2D_Platformer/Assets/Scripts/Common/Constants.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 public enum ItemType
 {
     //임의의 아이템이 나오길 원할 때는 Random을 사용
@@ -11,5 +12,29 @@ public enum ItemType
     Projectile,
     Star,
     Count
+
+}
+
+public class Constants
+{
+    public static readonly int MaxLevel = 10;
+    public static readonly int StarCount = 3;
+    //공통으로 사용하는 문자열 정보의 경우 변수를 선언해두고 사용하면 오타를 방지할 수 있고, 수정이 용이함
+    public static readonly string CurrentLevel = "CURRENT_LEVEL";
+    public static readonly string LevelUnlock = "LEVEL_UNLOCK_";
+    public static readonly string LevelStar = "LEVEL_START_";
+
+    public static (bool, bool[]) LoadLevelData(int level)
+    {
+        bool isUnlock = PlayerPrefs.GetInt($"{LevelUnlock}{level}", 0) == 1 ? true : false;
+        bool[] stars = new bool[StarCount];
+
+        for (int index = 0; index < StarCount; ++index)
+        {
+            stars[index] = PlayerPrefs.GetInt($"{LevelUnlock}{level}{index}", 0) == 1 ? true : false;
+        }
+
+        return (isUnlock, stars);
+    }
 
 }
---
+++ DoHyun/Unity2D_Platformer/Assets/Scripts/SceneControl/Utils.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public enum SceneNames { Intro = 0, SelectLeve1, Game, }
+
+public static class Utils
+{
+
+    public static string GetActiveScene()
+    {
+        return SceneManager.GetActiveScene().name;
+    }
+
+    public static void LoadScene(string sceneName = "")
+    {
+        if (sceneName == "")
+        {
+            SceneManager.LoadScene(GetActiveScene());
+        }
+        else
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+
+    }
+
+    public static void LoadScene(SceneNames sceneName)
+    {
+        //SceneNames를 열거형으로 매개변수를 받아온 경우 ToString() 처리
+        SceneManager.LoadScene(sceneName.ToString());
+    }
+
+}
